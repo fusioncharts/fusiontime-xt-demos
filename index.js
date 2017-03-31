22bfc2aedff041bf181beca29abe717bfe9c9711
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2017-03-31 17:41:49 +0530

    Added all 5  configs

---
+++ index.js
@@ -0,0 +1,50 @@
+import Web from './data/Web';
+import Apple from './data/Apple';
+import Mobile from './data/Mobile';
+import Slack from './data/Slack';
+import Ebola from './data/Ebola';
+
+const tsData = {
+	web: {
+		metadata: {
+			name: "Web Visits and Downloads",
+			desc: ""
+		},
+		data: Web
+	},
+
+	apple: {
+		metadata: {
+			name: "Apple Stock",
+			desc: ""
+		},
+		data: Apple
+	},
+
+	mobile: {
+		metadata: {
+			name: "Different Mobile OS",
+			desc: ""
+		},
+		data: Mobile
+	},
+
+	slack: {
+		metadata: {
+			name: "Slack Conversations",
+			desc: ""
+		},
+		data: Slack
+	},
+
+  ebola: {
+    metadata: {
+      name: 'Ebola Outbreak',
+      desc: ''
+    },
+    data: Ebola
+  },
+
+};
+
+window.tsData = tsData;
