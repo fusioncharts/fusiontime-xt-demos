37d6377d813b0252b9f2276e5ce3c3170b49e3e9
Jimut Dhali <jimutdhali@LPMB-Kol-Jimut.local>
2017-03-31 16:28:40 +0530

    Added build system webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const moduleConfig = {
+  rules: [{
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/
+  }]
+};
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'index.js'
+  },
+  module: moduleConfig,
+  devtool: '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
+}
