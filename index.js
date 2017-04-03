e8f397ddb2f90d32c1b3d347bc360433ccf41ff4
Subrata Mal <subrata@fusioncharts.com>
2017-04-03 17:50:04 +0530

    ESLint added + Lint related fixes

--- index.js
+++ index.js
@@ -5,58 +5,56 @@ import Slack from './data/Slack';
 import Ebola from './data/Ebola';
 import Nasa from './data/Nasa';
 
-
 const tsData = {
-	web: {
-		metadata: {
-			name: 'Web Visits and Downloads',
-			desc: 'Simple line series'
-		},
-		data: Web
-	},
+  web: {
+    metadata: {
+      name: 'Web Visits and Downloads',
+      desc: 'Simple line series',
+    },
+    data: Web,
+  },
 
-	apple: {
-		metadata: {
-			name: 'Apple Stock',
-			desc: 'Candle stick time series chart'
-		},
-		data: Apple
-	},
+  apple: {
+    metadata: {
+      name: 'Apple Stock',
+      desc: 'Candle stick time series chart',
+    },
+    data: Apple,
+  },
 
-	mobile: {
-		metadata: {
-			name: 'Different Mobile OS',
-			desc: 'Manipulate data'
-		},
-		data: Mobile
-	},
+  mobile: {
+    metadata: {
+      name: 'Different Mobile OS',
+      desc: 'Manipulate data',
+    },
+    data: Mobile,
+  },
 
-	slack: {
-		metadata: {
-			name: 'Slack Conversations',
-			desc: 'Fetch data from a JSON URL'
-		},
-		data: Slack
-	},
+  slack: {
+    metadata: {
+      name: 'Slack Conversations',
+      desc: 'Fetch data from a JSON URL',
+    },
+    data: Slack,
+  },
 
   ebola: {
     metadata: {
       name: 'Ebola Outbreak',
-      desc: 'Fetch and compare three differnet examples'
+      desc: 'Fetch and compare three differnet examples',
     },
-    data: Ebola
+    data: Ebola,
   },
 
   nasa: {
-  	metadata: {
-  		name: 'Global Temperature Avg',
-  		desc: 'GLOBAL LAND-OCEAN TEMPERATURE INDEX'
-  	},
-  	data: Nasa
-  }
-
+    metadata: {
+      name: 'Global Temperature Avg',
+      desc: 'GLOBAL LAND-OCEAN TEMPERATURE INDEX',
+    },
+    data: Nasa,
+  },
 };
 
-window.tsData = tsData;
+// window.tsData = tsData;
 
 export default tsData;
